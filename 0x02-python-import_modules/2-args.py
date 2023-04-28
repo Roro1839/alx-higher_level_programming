487b2abd4e8a1fcc39d03c7d48073aa0a7b8129f
Roro1839 <ronaldokipkirui90@gmail.com>
2023-04-28 08:49:14 -0700

    import module

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     import sys
-count = len(sys.argv)
-if count == 0:
-    print("0 arguments.")
-elif count == 1:
-    print("1 argument:")
-else:
-    print("{} arguments:".format(count))
-    for i in range(len(sys.argv)):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    count = len(sys.argv)
+    if count == 1:
+        print("0 arguments.")
+    elif count == 2:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count - 1))
+    for i in range(1, count):
+        print("{}: {}".format(i, sys.argv[i]))
